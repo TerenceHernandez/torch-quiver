61da2fb84f2a3153405d09a11948a949fa746a0a
TerenceHernandez <t.na.m.hernandez@gmail.com>
2022-06-08 13:11:00 +0100

    Debug Adj: separated adj inputs 7

--- examples/multi_gpu/pyg/reddit/dist_sampling_ogb_reddit_pyg_gpipe.py
+++ examples/multi_gpu/pyg/reddit/dist_sampling_ogb_reddit_pyg_gpipe.py
@@ -232,7 +232,7 @@ def run(rank, world_size, data_split, edge_index, x, y, num_features, num_classe
 				for adj in adjs:
 					print(adj.size[1])
 
-			sizes = [adj.sizes[1] for adj in adjs]
+			sizes = [adj.size[1] for adj in adjs]
 			sizes = [size.to(rank) for size in sizes]
 
 			adjs = [adj.edge_index for adj in adjs]
