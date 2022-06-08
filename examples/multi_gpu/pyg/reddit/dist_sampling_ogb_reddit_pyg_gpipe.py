716916f79682ff6a0a3d03646deca823514b83cf
TerenceHernandez <t.na.m.hernandez@gmail.com>
2022-06-08 15:04:50 +0100

    Debug Pipe: without pipe

--- examples/multi_gpu/pyg/reddit/dist_sampling_ogb_reddit_pyg_gpipe.py
+++ examples/multi_gpu/pyg/reddit/dist_sampling_ogb_reddit_pyg_gpipe.py
@@ -218,7 +218,7 @@ def run(rank, world_size, data_split, edge_index, x, y, num_features, num_classe
 	if rank == 0:
 		print(model)
 
-	model = GPipe(model, balance=[1, 2, 2], chunks=1, checkpoint='never')
+	# model = GPipe(model, balance=[1, 2, 2], chunks=1, checkpoint='never')
 
 	optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
