7073e67acdb9ad6f3c30d963412d5786a08410ba
TerenceHernandez <t.na.m.hernandez@gmail.com>
2022-06-09 19:33:30 +0100

    Plots update

--- plots/end_to_end/end_to_end_plots.py
+++ plots/end_to_end/end_to_end_plots.py
@@ -331,26 +331,26 @@ def convert():
 					 8.119239807128906, 8.578034162521362, 8.185813903808594, 8.172889709472656, 8.523643493652344,
 					 8.275611877441406, 8.479995012283325, 8.188637018203735, 18.493624448776245]
 
-	with open('data/4_gpu_sampling.csv', 'w') as out:
+	with open('single_node_data/4_gpu_sampling.csv', 'w') as out:
 		csv_out = csv.writer(out)
 		csv_out.writerow(['total_across_epoch', 'min', 'max'])
 		for row in sampling:
 			csv_out.writerow(row)
 
-	with open('data/4_gpu_features.csv', 'w') as out:
+	with open('single_node_data/4_gpu_features.csv', 'w') as out:
 		csv_out = csv.writer(out)
 		csv_out.writerow(['total_across_epoch', 'min', 'max'])
 		for row in feature:
 			csv_out.writerow(row)
 
-	with open('data/4_gpu_training.csv', 'w') as out:
+	with open('single_node_data/4_gpu_training.csv', 'w') as out:
 		csv_out = csv.writer(out)
 		csv_out.writerow(['total_across_epoch', 'min', 'max'])
 		for row in training:
 			csv_out.writerow(row)
 
 	x = pd.DataFrame(epoch)
-	x.to_csv('data/4_gpu_epoch.csv', index=False)
+	x.to_csv('single_node_data/4_gpu_epoch.csv', index=False)
 
 
 def read_csv(file_name, uses_headers=True, usecols=None):
@@ -360,7 +360,7 @@ def read_csv(file_name, uses_headers=True, usecols=None):
 def end_end_stats_composition(stat='sampling'):
 	ax = None
 	for i in range(1, 4):
-		df = read_csv(f'data/{i}_gpu_{stat}.csv', usecols=['total_across_epoch'])
+		df = read_csv(f'single_node_data/{i}_gpu_{stat}.csv', usecols=['total_across_epoch'])
 		ax = df.plot(y='total_across_epoch', kind='line', ax=ax, label=f'{i} gpu{""if i == 1 else "s"}')
 
 	ax.set_title(f"Time spent {keyword_map[stat]}")
@@ -373,9 +373,10 @@ def end_end_stats_composition(stat='sampling'):
 def end_end_stats():
 	ax = None
 	for i in range(1, 4):
-		df = read_csv(f'data/{i}_gpu_epoch.csv', )
-		ax = df.plot(kind='line', ax=ax, label=f'{i} gpu{"" if i == 1 else "s"}')
+		df = read_csv(f'single_node_data/{i}_gpu_epoch.csv', )
+		ax = df.plot(kind='line', ax=ax)
 
+	ax.legend([f'{i} gpu{"" if i == 1 else "s"}' for i in range(1, 4)])
 	ax.set_title(f"Time spent in total")
 	ax.set_xlabel("Epoch")
 	ax.set_ylabel("Time in s")
@@ -388,7 +389,7 @@ def percentage_time_spent():
 		operations = []
 		label_vals = []
 		for label in keyword_map.keys():
-			df = read_csv(f'data/{i}_gpu_{label}.csv', usecols=['total_across_epoch'])
+			df = read_csv(f'single_node_data/{i}_gpu_{label}.csv', usecols=['total_across_epoch'])
 			operations.append(df['total_across_epoch'].sum())
 			label_vals.append(keyword_map[label].capitalize())
 
@@ -404,7 +405,7 @@ def percentage_time_spent():
 						startangle=90)
 		ax1.axis('equal')
 
-		ax1.set_title(f"Percentage time spent on each operation")
+		ax1.set_title(f"Percentage time spent on each operation using {i} GPUs")
 		plt.tight_layout()
 		plt.show()
 
